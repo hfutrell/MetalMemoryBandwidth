4e6c7141d7d1e95fc4a142ad4c0f27e890063407
Holmes Futrell <holmes@gingerlabs.com>
2016-06-17 19:28:40 -0700

    stub Mac project.

---
+++ Mac/MetalMemoryBandwidth/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MetalMemoryBandwidth
+//
+//  Created by Holmes Futrell on 6/17/16.
+//  Copyright © 2016 Holmes Futrell. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
