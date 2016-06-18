d6b8255d2b668d8de5ee562b480832783955d298
Holmes Futrell <holmes@gingerlabs.com>
2016-06-18 13:33:32 -0700

    execute multiple iterations.

--- Mac/MetalMemoryBandwidth/main.swift
+++ Mac/MetalMemoryBandwidth/main.swift
@@ -39,17 +39,20 @@ func computeSumMetal(array1 array1 : [Float], array2 : [Float], inout sumArray :
         print(device);
     }
     
-    let defaultDevice : MTLDevice = MTLCreateSystemDefaultDevice()!;
+    let defaultDevice : MTLDevice = devices[0];
     print("default device = ", defaultDevice);
     
-    let commandQueue = defaultDevice.newCommandQueue();
-    let commandBuffer = commandQueue.commandBuffer();
-    let commandEncoder = commandBuffer.computeCommandEncoder();
+    // create the buffers
+    let resourceOptions: MTLResourceOptions = MTLResourceOptions.StorageModeShared
+    let arrayBuffer1 = defaultDevice.newBufferWithBytes(array1, length: N * sizeof(Float), options: resourceOptions)
+    let arrayBuffer2 = defaultDevice.newBufferWithBytes(array2, length: N * sizeof(Float), options: resourceOptions)
+    let sumBuffer = defaultDevice.newBufferWithBytes(sumArray, length: N * sizeof(Float), options: resourceOptions)
     
+    // create the library
     let library: MTLLibrary = generateLibrary(defaultDevice)
-    
     let myAdditionFunction: MTLFunction = library.newFunctionWithName("myAddFunction")!;
     
+    // create compute pipeline state
     let computePipelineState: MTLComputePipelineState?
     do {
         computePipelineState = try defaultDevice.newComputePipelineStateWithFunction(myAdditionFunction);
@@ -59,60 +62,69 @@ func computeSumMetal(array1 array1 : [Float], array2 : [Float], inout sumArray :
         return;
     }
 
-    // 1. Call the setComputePipelineState(_:) method with the MTLComputePipelineState object that contains the compute function that will be executed.
-    commandEncoder.setComputePipelineState(computePipelineState!);
+    let commandQueue = defaultDevice.newCommandQueue();
     
-    // 2 Specify resources that hold the input data (or output destination) for the compute function. Set the location (index) of each resource in its corresponding argument table.
-    let resourceOptions: MTLResourceOptions = MTLResourceOptions.StorageModeManaged
-    
-    let arrayBuffer1 = defaultDevice.newBufferWithBytes(array1, length: N * sizeof(Float), options: resourceOptions)
-    let arrayBuffer2 = defaultDevice.newBufferWithBytes(array2, length: N * sizeof(Float), options: resourceOptions)
-    let sumBuffer = defaultDevice.newBufferWithBytes(sumArray, length: N * sizeof(Float), options: resourceOptions)
-    
-    commandEncoder.setBuffer(arrayBuffer1, offset: 0, atIndex: 0);
-    commandEncoder.setBuffer(arrayBuffer2, offset: 0, atIndex: 1);
-    commandEncoder.setBuffer(sumBuffer, offset: 0, atIndex: 2);
-
-    // 3. Call the dispatchThreadgroups(_:threadsPerThreadgroup:) method to encode the compute function with a specified number of threadgroups for the grid and the number of threads per threadgroup.
     let threadsPerThreadgroup: MTLSize = MTLSizeMake(256, 1, 1) // a decent guess
     let threadGroups: MTLSize = MTLSizeMake(N / threadsPerThreadgroup.width, 1, 1)
-    commandEncoder.dispatchThreadgroups(threadGroups, threadsPerThreadgroup: threadsPerThreadgroup)
     
-    // 4. Call endEncoding() to finish encoding the compute commands onto the command buffer.
-    commandEncoder.endEncoding();
+    print("generating command buffers ...")
     
-    
-    commandBuffer.enqueue();
-    commandBuffer.commit(); // causes the command buffer to be executed as soon as possible
-    
-    print("command buffer committed");
-    
-    commandBuffer.waitUntilScheduled();
-
     let timeStart = NSDate()
+    let generateCommandBuffersStart: NSDate = NSDate()
+    
+    let iterations = 100
+    var lastCommandBuffer: MTLCommandBuffer? = nil
+    for _ in 0..<iterations {
+        
+        let commandBuffer = commandQueue.commandBuffer();
+        let commandEncoder = commandBuffer.computeCommandEncoder();
+        
+        // 1. Call the setComputePipelineState(_:) method with the MTLComputePipelineState object that contains the compute function that will be executed.
+        commandEncoder.setComputePipelineState(computePipelineState!);
+        
+        // 2 Specify resources that hold the input data (or output destination) for the compute function. Set the location (index) of each resource in its corresponding argument table.
+        commandEncoder.setBuffer(arrayBuffer1, offset: 0, atIndex: 0);
+        commandEncoder.setBuffer(arrayBuffer2, offset: 0, atIndex: 1);
+        commandEncoder.setBuffer(sumBuffer, offset: 0, atIndex: 2);
 
-    print("command buffer scheduled");
+        // 3. Call the dispatchThreadgroups(_:threadsPerThreadgroup:) method to encode the compute function with a specified number of threadgroups for the grid and the number of threads per threadgroup.
+        commandEncoder.dispatchThreadgroups(threadGroups, threadsPerThreadgroup: threadsPerThreadgroup)
+        
+        // 4. Call endEncoding() to finish encoding the compute commands onto the command buffer.
+        commandEncoder.endEncoding();
+        
+        commandBuffer.enqueue();
+        commandBuffer.commit(); // causes the command buffer to be executed as soon as possible
+        
+        commandBuffer.waitUntilScheduled()
+        
+        lastCommandBuffer = commandBuffer
+    }
     
-    commandBuffer.waitUntilCompleted();
+    print("took ", NSDate().timeIntervalSinceDate(generateCommandBuffersStart), " seconds");
     
+    lastCommandBuffer!.waitUntilCompleted();
     print("command buffer complete.");
     
+//    if (lastCommandBuffer!.status == MTLCommandBufferStatus.Error) {
+//        if (lastCommandBuffer!.error != nil) {
+//            print("command buffer failed with error: ", lastCommandBuffer!.error)
+//        }
+//        else {
+//            print("command buffer failed")
+//            
+//        }
+//        return
+//    }
+    
     let timeInterval: Double = NSDate().timeIntervalSinceDate(timeStart)
-    let bytesPerSecond : Double = (Double(3 * N * sizeof(Float))) / Double(timeInterval)
+    let bytesPerSecond : Double = (Double(iterations * 3 * N * sizeof(Float))) / Double(timeInterval)
     let gBytesPerSecond : Double = bytesPerSecond / 1024.0 / 1024.0 / 1024.0
     
     print("took ", timeInterval, " seconds")
     print("bytes per second: ", bytesPerSecond)
     print("GB per second: ", gBytesPerSecond)
 
-    if (commandBuffer.status == MTLCommandBufferStatus.Error) {
-        if (commandBuffer.error != nil) {
-            print("command buffer failed with error: ", commandBuffer.error)
-        }
-        else {
-            print("command buffer failed")
-        }
-    }
     
     let output : UnsafeMutablePointer<Float> = UnsafeMutablePointer<Float>(sumBuffer.contents())
     
@@ -122,7 +134,7 @@ func computeSumMetal(array1 array1 : [Float], array2 : [Float], inout sumArray :
 
 // create an array of random floats
 
-var N : Int = 1024 * 1024 * 20;
+var N : Int = 1024 * 1024 * 10;
 
 print("Running test with N = ", N);
 
