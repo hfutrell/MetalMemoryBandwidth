1ff41f51a42b57dd0f68f29f3138d917ebc91c6a
Holmes Futrell <holmes@gingerlabs.com>
2016-06-17 19:57:56 -0700

    basic CPU addition. Next up, Metal.

--- Mac/MetalMemoryBandwidth/main.swift
+++ Mac/MetalMemoryBandwidth/main.swift
@@ -8,5 +8,48 @@
 
 import Foundation
 
-print("Hello, World!")
+func randomFloat() -> Float {
+    return Float(arc4random()) / Float(UINT32_MAX);
+}
 
+func computeSumCPU(array1 array1 : [Float], array2 : [Float], inout sumArray : [Float], N : Int) {
+    for i in 0..<N {
+        sumArray[i] = array1[i] + array2[i];
+    }
+}
+
+// create an array of random floats
+
+var N : Int = 1024 * 1024;
+
+print("Running test with N = ", N);
+
+var floatArray1 = [Float](count: N, repeatedValue: 0.0);
+var floatArray2 = [Float](count: N, repeatedValue: 0.0);
+var sumArray = [Float](count: N, repeatedValue: 0.0);
+
+for i in 0..<N {
+    floatArray1[i] = randomFloat();
+}
+for i in 0..<N {
+    floatArray2[i] = randomFloat();
+}
+
+// compute sum
+
+computeSumCPU(array1: floatArray1, array2: floatArray2, sumArray: &sumArray, N: N);
+
+// verify sum
+
+var success = true;
+for i in 0..<N {
+    if sumArray[i] != floatArray1[i] + floatArray2[i] {
+        print("sum failed at index", i);
+        success = false;
+        break;
+    }
+}
+
+if success {
+    print("success");
+}
